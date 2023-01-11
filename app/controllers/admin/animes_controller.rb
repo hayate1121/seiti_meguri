d6f2dd5622865058ef40ce1dc6ab830d2c5e4e4d
EC2 Default User <ec2-user@ip-172-31-6-240.ap-northeast-1.compute.internal>
2023-01-11 15:11:26 +0000

    0111[seiti]

--- app/controllers/admin/animes_controller.rb
+++ app/controllers/admin/animes_controller.rb
@@ -6,7 +6,7 @@ class Admin::AnimesController < ApplicationController
   def create
     anime = Anime.new(anime_params)
     anime.save
-    redirect_to root_path(anime.id)
+    redirect_to admin_anime_path(anime.id)
   end
   
   def show
@@ -22,7 +22,7 @@ class Admin::AnimesController < ApplicationController
   def update
     anime = Anime.find(params[:id])
     anime.update(anime_params)
-    redirect_to anime_path(anime.id)
+    redirect_to admin_anime_path(anime.id)
   end
   
   def index
@@ -32,7 +32,7 @@ class Admin::AnimesController < ApplicationController
   private
   
   def anime_params
-    params.require(:anime).permit(:name, :spot)
+    params.require(:anime).permit(:title, :spot, :anime_image)
   end
   
 end
--- app/models/anime.rb
+++ app/models/anime.rb
@@ -5,4 +5,12 @@ class Anime < ApplicationRecord
   has_many :scenes, dependent: :destroy
   has_many :stores, dependent: :destroy
   
+  def get_anime_image(width, height)
+    unless anime_image.attached?
+      file_path = Rails.root.join('app/assets/images/noimage-760x460.jpg')
+      anime_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    anime_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
 end
