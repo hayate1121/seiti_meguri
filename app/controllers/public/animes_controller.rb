c321e5c647b04fea7ba6e0f34f27243aadd3d73b
EC2 Default User <ec2-user@ip-172-31-6-240.ap-northeast-1.compute.internal>
2023-02-01 13:37:42 +0000

    0201[publicレイアウト]

--- app/controllers/public/animes_controller.rb
+++ app/controllers/public/animes_controller.rb
@@ -1,10 +1,23 @@
 class Public::AnimesController < ApplicationController
   def index
-    @animes = Anime.all
+    @animes = params[:tag_id].present? ? Tag.find(params[:tag_id]).animes.order("title") : Anime.order("title").all
   end
   
   def show
     @anime = Anime.find(params[:id])
     @scenes = @anime.scenes
+    @stores = Store.all
+    if @scenes.present?
+      @lat = 0
+      @lng = 0
+      @count = 0
+      @scenes.each do |scene|
+        @lat += scene.latitude
+        @lng += scene.longitude
+        @count += 1
+      end
+      @latitude = @lat/@count
+      @longitude = @lng/@count
+    end
   end
 end
